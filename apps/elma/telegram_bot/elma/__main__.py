facc412e6e92cd0961219430f38decdef6c1094a
MagonxESP <janma.360@gmail.com>
2022-03-16 19:24:07 +0100

    remove unused message filters

--- apps/elma/telegram_bot/elma/__main__.py
+++ apps/elma/telegram_bot/elma/__main__.py
@@ -1,20 +1,14 @@
-from telegram import Update
 from telegram.ext import CallbackContext, MessageHandler, Filters
 import telegram.ext
 from elma.handlers import add_handlers
 import elma.settings as settings
 
+
 telegram_bot_updater = telegram.ext.Updater(token=settings.TELEGRAM_BOT_TOKEN)
-
-
-def stickers(update: Update, context: CallbackContext):
-    text = update.message.sticker.file_id
-    print(text)
 
 
 def main():
     add_handlers(telegram_bot_updater.dispatcher)
-    telegram_bot_updater.dispatcher.add_handler(MessageHandler(Filters.sticker, stickers))
 
     telegram_bot_updater.start_polling()
     telegram_bot_updater.idle()
